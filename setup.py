8498f8776a14a838bcf5eb895227a99c9313c1ad
sanders76 <s.teunissen@gmail.com>
2016-11-11 13:56:11 +0100

    adding setup stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='markdown-columns',
+    version='0.1',
+    packages=['md_columns'],
+    url='',
+    license='MIT',
+    author='steun',
+    author_email='',
+    description=''
+)
